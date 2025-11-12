37413e1ab9f245dc90ab6a925a63ffc547d96ab7
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-12 18:26:54 +0900

    update

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -1,7 +1,27 @@
-<div>
-    <h1>商品詳細</h1>
-    <h2>ID</h2>
-    <p>{{ $id }}</p>
-    <h2>メッセージ</h2>
-    <p>{{ $message }}</p>
-</div>
+<!-- TODO: レイアウト指定(extends): layouts/app.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+<!-- TODO: メインコンテンツ(section): content -->
+    <main class="container mx-auto px-4 pt-6">
+        <section class="flex">
+            <div class="w-1/2 rounded-lg overflow-hidden">
+                <img src="/images/Oxford-Shirt.png" class="">
+            </div>
+            <div class="w-1/2 pl-6 flex flex-col justify-between">
+                <div>
+                    <div class="info p-3 space-y-3">
+                        <h3 class="name font-semibold text-2xl">Oxford Shirt</h3>
+                        <p class="note text-gray-500">Oxford Shirt</p>
+                        <p class="price font-bold text-2xl">&yen;4,500</p>
+                    </div>
+                </div>
+                <div class="text-center border-t">
+                    <button class="add-cart-btn text-white text-lg bg-sky-500 px-3 py-2 w-full rounded-lg">
+                        Add Cart
+                    </button>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-// Requestクラスをインポート
-use Illuminate\Http\Request;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ItemController;
 
-// HomeController の index を実行
 Route::get('/', [HomeController::class, 'index']);
-// HomeController の about を実行
 Route::get('/about', [HomeController::class, 'about']);
-// HomeController の search を実行
 Route::get('/search', [HomeController::class, 'search']);
-// ItemController の show を実行: id を渡す
-Route::get('/item/{id}', [ItemController::class, 'show']);
-
-// GETリクエストで、トップページにアクセスしたときのルーティング
-// Route::get('/', function () {
-//     // resources/views/home/index.blade.php を表示する
-//     return view('home.index');
-// });
-// /about でアクセスしたときのルーティング
-// Route::get('/about', function () {
-//     // resources/views/about.blade.php を表示する
-//     return view('about');
-// });
-
-// /item/{id} でアクセスしたときのルーティング
-// Route::get('/item/{id}', function (int $id) {
-//     $message = "Item id is {$id}";
-//     // resources/views/item/show.blade.php を表示する
-//     // データ作成
-//     $data = [
-//         'message' => $message,
-//         'id' => $id
-//     ];
-//     // ビューにデータを渡す
-//     return view('item.show', $data);
-// });
-
-// /search でアクセスしたときのルーティング
-// Route::get('/search', function (Request $request) {
-//     // $keyword = $_GET['keyword'] ?? 'No keyword';
-//     // dd(): デバッグ関数で強制終了する
-//     // dd($request->all());
-//     // $keyword = $request->input('keyword', 'No keyword');
-//     $keyword = $request->keyword ?? 'キーワードが指定されていません';
-//     $date = [
-//         'keyword' => $keyword,
-//         'result' => '何も見つかりませんでした',
-//     ];
-//     // http://127.0.0.1:8000/search?keyword=xxx
-//     // にアクセスすると keyword に応じたデータが表示される
-//     return view('search', $date);
-// });
+Route::get('/item/show/{id}', [ItemController::class, 'show']);
