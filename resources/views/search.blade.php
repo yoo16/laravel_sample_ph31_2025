d4ec064152bd6529582c260aa18a1a4991ad5f77
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-12 18:21:16 +0900

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,18 @@
-<div>
-    <h1>検索結果</h1>
-    <h2>キーワード</h2>
-    <p>{{ $keyword }}</p>
-    <h2>結果</h2>
-    <p>{{ $result }}</p>
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <main class="container mx-auto">
+        <div>
+            <h1 class="text-2xl text-center p-2">検索結果</h1>
+            <p>{{ $result }}</p>
+        </div>
+
+        <section id="product-section" class="my-4">
+            <!-- TODO: フィルターボタンと検索フォーム: components/product-nav.blade.php -->
+
+            <div id="product-list" class="mt-4 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                ここに商品カードのリストが表示
+            </div>
+        </section>
+    </main>
+@endsection
