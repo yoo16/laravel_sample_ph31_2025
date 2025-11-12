26075236a8d7e47c49356be3c97d80d342a7274b
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-12 16:40:56 +0900

    update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- TODO: headタグ: componens/head.blade.php -->
+
+<body class="min-h-screen">
+
+    <!-- TODO: ヘッダナビゲーション(include): componens/nav.blade.php -->
+
+    <!-- TODO: メインコンテンツ(yield): content -->
+</body>
+
+</html>
