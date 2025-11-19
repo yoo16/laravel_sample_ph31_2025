8935607e98946f7158452def82ea5df9e739afee
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-19 16:35:56 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,12 @@ Route::get('/about', [HomeController::class, 'about']);
 Route::get('/search', [HomeController::class, 'search']);
 
 // ItemControllerのshowメソッドを呼び出すルートを定義
+// ルート名：item.show
 Route::get('/item/show/{id}', [ItemController::class, 'show'])
     ->name('item.show');
+// ルート名：item.edit
+Route::get('/item/edit/{id}', [ItemController::class, 'edit'])
+    ->name('item.edit');
+// ルート名：item.index
+Route::get('/item/', [ItemController::class, 'index'])
+    ->name('item.index');
