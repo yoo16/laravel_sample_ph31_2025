4953a7db278ba1010457960230414991822a086d
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-26 17:53:12 +0900

    update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'note',
+        'image',
+        'tag',
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
