bcf3e1774e969eb8254208b092a9997469b108f6
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-26 17:09:56 +0900

    update

--- database/migrations/2025_11_26_080145_create_items_table.php
+++ database/migrations/2025_11_26_080145_create_items_table.php
@@ -14,7 +14,28 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('name', 100);
+            $table->text('note');
+            $table->integer('price');
+            $table->text('image')->nullable();
+            $table->text('tag')->nullable();
         });
+
+        // CREATE TABLE `items` (
+        //   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+        //   `created_at` timestamp NULL DEFAULT NULL,
+        //   `updated_at` timestamp NULL DEFAULT NULL,
+        //   `category_id` bigint unsigned NOT NULL,
+        //   `name` varchar(100) NOT NULL,
+        //   `note` text NOT NULL,
+        //   `price` int NOT NULL,
+        //   `image` text DEFAULT NULL,
+        //   `tag` text DEFAULT NULL,
+        //   PRIMARY KEY (`id`),
+        //   KEY `items_category_id_foreign` (`category_id`),
+        //   CONSTRAINT `items_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+        // ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8
     }
 
     /**
