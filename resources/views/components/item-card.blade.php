8361634eb088da39c38d37628ec80aed1e2206a0
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-12-03 17:40:11 +0900

    update

--- resources/views/components/item-card.blade.php
+++ resources/views/components/item-card.blade.php
@@ -2,7 +2,7 @@
     class="product bg-white rounded-xl shadow cursor-pointer
                         hover:shadow-lg transition transform hover:-translate-y-1 ${cardClass}">
     <a href="{{ route('item.show', $item->id) }}">
-        <img src="/images/Oxford-Shirt.png" class="rounded-t-md">
+        <img src="{{ $item->image }}" class="rounded-t-md">
         <div class="info p-3">
             <h3 class="name font-semibold text-lg">{{ $item->name }}</h3>
             <p class="note text-sm text-gray-500">{{ $item->note }}</p>
