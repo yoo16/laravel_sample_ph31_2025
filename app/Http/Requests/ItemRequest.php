aae3e6ece883ba4bead09738d690a2fa15eebd7a
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-12-17 16:35:05 +0900

    update

--- app/Http/Requests/ItemRequest.php
+++ app/Http/Requests/ItemRequest.php
@@ -6,23 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ItemRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'price' => 'required|integer',
+            'note' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'image' => 'nullable|image|max:1024',
         ];
     }
 }
